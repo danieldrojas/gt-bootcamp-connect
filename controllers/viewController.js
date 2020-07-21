a96992f6d29991aa4de8951a7d82f747967046ed
NCM <nmitchell34@gatech.edu>
2020-07-21 12:01:13 -0400

    adding to viewController for profile page loading

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -3,53 +3,50 @@ const router = express.Router();
 const db = require("../models");
 
 router.get("/", (req, res) => {
-    res.render("login");
+  res.render("login");
 });
 
 router.get("/signup", function (req, res) {
-    res.render("signup");
+  res.render("signup");
 });
 
 // Navigate to profile of specific person
 router.get("/profile/:id", function (req, res) {
-    console.log("Loading Profile Page");
-    // console.log(req.params);
-    db.User.findOne({
-        where: {
-            id: req.params.id,
-        },
-        include: [post],
-    })
-    }).then(function (userResponse) {
-        console.log(userResponse.dataValues);
-        var hbsObject = {
-            profileUser: userResponse.dataValues,
-        };
-        console.log(hbsObject);
-        res.render("profile", hbsObject);
-    });
+  console.log("Loading Profile Page");
+  // console.log(req.params);
+  db.User.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [post],
+  }).then(function (userResponse) {
+    console.log(userResponse.dataValues);
+    var hbsObject = {
+      profileUser: userResponse.dataValues,
+    };
+    console.log(hbsObject);
+    res.render("profile", hbsObject);
+  });
 });
 
 // router.get("/dashboard", function(req,res){ // if turn on i will overwrite my api
 //   res.render("dashboard")
 // })
 router.get("/post", function (req, res) {
-    res.render("post");
+  res.render("post");
 });
 router.get("/dashboard", function (req, res) {
-    res.render("dashboard");
+  res.render("dashboard");
 });
 
-
-
 router.get("/edit_profile/:id", function (req, res) {
-    db.User.findOne({
-        where: {
-            id: req.params.id,
-        },
-    }).then((data) => {
-        res.render("edit_profile", data.dataValues);
-    });
+  db.User.findOne({
+    where: {
+      id: req.params.id,
+    },
+  }).then((data) => {
+    res.render("edit_profile", data.dataValues);
+  });
 });
 
 module.exports = router;
